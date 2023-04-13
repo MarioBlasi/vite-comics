f956597cbfb4f815adafb2dba3cfb69d93163e8f
MarioBlasi <mblasi509@gmail.com>
2023-04-13 14:40:11 +0100

    struttura col

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,80 @@
 <script>
 export default {};
 </script>
+<!-- JS ---------------->
 
-<template></template>
+<template>
+  <div class="navTop-level">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>sono top nav</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- nav top level -->
+  <div class="navMedium-level">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>sono medium nav</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- nav medium level -->
+  <div class="navBottom-level">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>sono bottom nav</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- nav botto level -->
 
-<style lang="scss" scoped></style>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>MAIN</h1>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- main -->
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>FOOTER</h1>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer -->
+</template>
+<!-- HTML ---------------->
+<style lang="scss" scoped>
+.navTop-level {
+  background-color: cadetblue;
+}
+.navMedium-level {
+  background-color: rgb(208, 235, 183);
+}
+.navBottom-level {
+  background-color: rgb(177, 62, 133);
+}
+// nav * 3
+main {
+  background-color: rgb(81, 177, 62);
+  height: 10rem;
+}
+footer {
+  background-color: rgba(39, 44, 38, 0.466);
+}
+</style>
+<!-- CSS ----------------->
