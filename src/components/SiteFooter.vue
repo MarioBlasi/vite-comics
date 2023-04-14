1806c955dffe33be51d3cd261ef37142fa2f4eb5
MarioBlasi <mblasi509@gmail.com>
2023-04-14 18:08:10 +0100

    footer top

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Sitefooter",
+};
+</script>
+<!-- JS ---------------->
+
+<template></template>
+<!-- HTML -->
+<style lang="scss">
+h3 {
+  background-color: aqua;
+}
+</style>
+CSS
